68aa883cb085b95d89ffb2b39894872fb5dad4b6
robert schroeder <r_schroeder_r@hotmail.com>
2018-08-06 19:40:33 +0200

    Agregado CORS

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var colors = require('colors');
 // Inicializar variables
 var app = express();
 
+// Cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
 // Body Parser
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
